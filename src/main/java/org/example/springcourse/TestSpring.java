e4f42e5b12cef5ca68f4d5d7dd7d30b8e6b8a550
mukhametzyanovra <rikitavi777@yandex.ru>
2023-06-20 20:59:17 +0300

    Initial commit

---
+++ src/main/java/org/example/springcourse/TestSpring.java
@@ -0,0 +1,14 @@
+package org.example.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Music musicBean = context.getBean("musicBean", Music.class);
+        MusicPlayer musicPlayer = new MusicPlayer(musicBean);
+        musicPlayer.playMusic();
+        context.close();
+    }
+}
