5c3aaea64eed933caf3b71e774381785dc115844
mukhametzyanovra <rikitavi777@yandex.ru>
2023-06-22 20:24:11 +0300

    Путь к проекту указан в конф файле чтобы спринг сам сканировал код

--- src/main/java/org/example/springcourse/ClassicalMusic.java
+++ src/main/java/org/example/springcourse/ClassicalMusic.java
@@ -1,8 +1,28 @@
 package org.example.springcourse;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClassicalMusic implements Music {
+
+    private ClassicalMusic() {
+
+    }
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
     @Override
     public String getSong() {
         return "Времена года";
+    }
+
+    public void doMyInit() {
+        System.out.println("Doing initialization!");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Doing destroy!");
     }
 }
--- src/main/java/org/example/springcourse/TestSpring.java
+++ src/main/java/org/example/springcourse/TestSpring.java
@@ -6,10 +6,9 @@ public class TestSpring {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-//        Music musicBean = context.getBean("musicBean", Music.class);
-//        MusicPlayer musicPlayer = new MusicPlayer(musicBean);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusicList();
+        Music rapMusic = context.getBean("rapMusic", Music.class);
+        MusicPlayer musicPlayer = new MusicPlayer(rapMusic);
+        musicPlayer.playMusic();
         context.close();
     }
 }
